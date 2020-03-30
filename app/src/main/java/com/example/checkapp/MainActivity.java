d740591c9e4003f663e0ba8d7e0c91ce1287010c
unknown <u1065204@hk.edu.tw>
2020-03-30 11:52:25 +0800

    3

--- app/src/main/java/com/example/checkapp/MainActivity.java
+++ app/src/main/java/com/example/checkapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.checkapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnOK(View view) {
+        CheckBox chk;
+        TextView txvResult;
+        String msg = "" ;
+
+        int[] id = {R.id.checkBox,R.id.checkBox2,R.id.checkBox3,R.id.checkBox4};
+
+        for(int i:id){
+            chk=(CheckBox)findViewById(i);
+
+            if(chk.isChecked())
+                msg += "\n" + chk.getText();
+        }
+
+        txvResult = (TextView)findViewById(R.id.textView2);
+        txvResult.setText(msg);
     }
 }
